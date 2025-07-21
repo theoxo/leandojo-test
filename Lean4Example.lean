dcb9120ee70b4e99b8d113b1f40168e8ffcea57b
Theo X. Olausson <theoxo@mit.edu>
2025-07-21 18:12:32 -0400

    Update Lean4Example.lean

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -1,7 +1,113 @@
-open Nat (add_assoc add_comm)
+theorem ex1_and_comm (p q : Prop) : p ∧ q → q ∧ p := by
+  intro h
+  cases h with | intro h1 h2 =>
+  constructor
+  exact h2
+  exact h1
 
-theorem hello_world (a b c : Nat)
-  : a + b + c = a + c + b := by
-  rw [add_assoc, add_comm b, ←add_assoc]
 
-theorem foo (a : Nat) : a + 1 = Nat.succ a := by rfl
+theorem ex2_or_comm (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h with
+  | inl h1 =>
+    right
+    exact h1
+  | inr h2 =>
+    left
+    exact h2
+
+theorem ex3_imp_trans (p q r : Prop) : (p → q) → (q → r) → (p → r) := by
+  intro h1
+  intro h2
+  intro h3
+  apply h2
+  apply h1
+  exact h3
+
+theorem ex4_and_distrib_or (p q r : Prop) : p ∧ (q ∨ r) → (p ∧ q) ∨ (p ∧ r) := by
+  intro h
+  cases h with
+  | intro h1 h2 =>
+    cases h2 with
+    | inl h3 =>
+      left
+      constructor
+      exact h1
+      exact h3
+    | inr h4 =>
+      right
+      constructor
+      exact h1
+      exact h4
+
+theorem ex5_de_morgan_and (p q : Prop) : ¬(p ∧ q) → ¬p ∨ ¬q := by
+  intro h1
+  by_cases h2 : p
+  right
+  intro h3
+  apply h1
+  constructor
+  exact h2
+  exact h3
+  left
+  exact h2
+
+theorem ex6_de_morgan_or (p q : Prop) : ¬(p ∨ q) → ¬p ∧ ¬q := by
+  intro h
+  constructor
+  intro h1
+  apply h
+  left
+  exact h1
+  intro h2
+  apply h
+  right
+  exact h2
+
+theorem ex7_iff_symm (p q : Prop) : (p ↔ q) → (q ↔ p) := by
+  intro h
+  constructor
+  intro h1
+  apply h.mpr
+  exact h1
+  intro h2
+  apply h.mp
+  exact h2
+
+theorem ex8_contrapositive (p q : Prop) : (p → q) → (¬q → ¬p) := by
+  intro h1
+  intro h2
+  intro h3
+  apply h2
+  apply h1
+  exact h3
+
+theorem ex9_or_distrib_and (p q r : Prop) : p ∨ (q ∧ r) → (p ∨ q) ∧ (p ∨ r) := by
+  intro h
+  cases h with
+  | inl h1 =>
+    constructor
+    left
+    exact h1
+    left
+    exact h1
+  | inr h2 =>
+    cases h2 with
+    | intro h3 h4 =>
+      constructor
+      right
+      exact h3
+      right
+      exact h4
+
+theorem ex10_absorbtion (p q : Prop) : p ∧ (p ∨ q) ↔ p := by
+  constructor
+  intro h
+  cases h with
+  | intro h1 _ =>
+    exact h1
+  intro h1
+  constructor
+  exact h1
+  left
+  exact h1
